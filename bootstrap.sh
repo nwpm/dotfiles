0327844c8791d6df92a63ec43e97cebf5fe0605c
nwpm <fwxgkt@gmail.com>
2026-02-14 13:35:28 +0300

    feat: Add colors in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,52 +1,58 @@
 #!/bin/sh
+
 set -e
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "====> Start setting dotfiles"
+echo "${GREEN}====> Start setting dotfiles${NC}"
 echo
 
 DOT_DIR=$(pwd)
 
 install_packages() {
-    echo "${RED}====> Install pacman packages${NC}..."
+    echo "${GREEN}====> Install pacman packages${NC}..."
     sudo pacman -Syu --needed - < "$DOT_DIR/packages/pacman.txt"
 }
 
 install_aur() {
-    echo "${RED}====> Install AUR packages${NC}..."
-    echo "${RED}====> Install yay${NC}..."
+    echo "${GREEN}====> Install yay${NC}..."
     git clone https://aur.archlinux.org/yay.git /tmp/yay && (cd /tmp/yay && makepkg -si --noconfirm)
+
+    echo "${GREEN}====> Install AUR packages${NC}..."
     yay -S --noconfirm --needed - < "$DOT_DIR/packages/aur.txt"
 }
 
 change_shell(){
-  echo "${RED}====> Change default shell to zsh${NC}..."
+  echo "${GREEN}====> Change default shell to zsh${NC}..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  sudo chsh -s /bin/zsh $(whoami)
+  sudo chsh -s /bin/zsh "$(whoami)"
 }
 
 set_up_home(){
-  echo "${RED}====> Set up home directory${NC}..."
-  cp "$DOT_DIR/home/.xinitrc" "$HOME"
+  echo "${GREEN}====> Set up home directory${NC}..."
+
   rm -f "$HOME/.zshrc"
+  rm -f "$HOME/.xinitrc"
+  rm -f "$HOME/.gitconfig"
+
+  cp "$DOT_DIR/home/.xinitrc" "$HOME"
   cp "$DOT_DIR/home/.zshrc" "$HOME"
   cp "$DOT_DIR/home/.gitconfig" "$HOME"
 }
 
 setting_sddm_theme(){
-  echo "${RED}====> Set up sddm theme${NC}..."
+  echo "${GREEN}====> Set up sddm theme${NC}..."
   sudo mkdir -p /etc/sddm.conf.d
   cp "$DOT_DIR/system/sddm/sddm.conf" "/etc/sddm.conf.d/"
 }
 
 add_user_in_groups(){
-  echo "${RED}====> Add user in groups${NC}..."
-  sudo usermod -aG libvirt $(whoami)
+  echo "${GREEN}====> Add user in groups${NC}..."
+  sudo usermod -aG libvirt "$(whoami)"
 }
 
 link_configs() {
-    echo "${RED}====> Link configs${NC}..."
+    echo "${GREEN}====> Link configs${NC}..."
     for dir in "$DOT_DIR/config/"*; do
         name=$(basename "$dir")
         rm -rf "$HOME/.config/$name"
@@ -55,17 +61,19 @@ link_configs() {
 }
 
 enable_services() {
-    echo "${RED}====> Enable services${NC}..."
+    echo "${GREEN}====> Enable services${NC}..."
     sudo systemctl enable sddm
     sudo systemctl enable libvirtd
 }
 
 set_up_languages(){
-   echo "${RED}====> Set up languages${NC}..."
-  sudo sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-  sudo sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-  sudo locale-gen
-  echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
+    echo "${GREEN}====> Set up languages${NC}..."
+
+    sudo sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+    sudo sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+    sudo locale-gen
+
+    echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
 }
 
 main() {
@@ -80,7 +88,7 @@ main() {
     set_up_languages
 
     echo
-    echo "DONE! The system is configured"
+    echo "DONE! The system is configured."
     echo
 }
 
